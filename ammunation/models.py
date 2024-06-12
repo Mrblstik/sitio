a2db3e246d497674743d3e299f17081d0eadf4d9
simon <simo.munoz@duocuc.cl>
2024-06-11 23:41:54 -0400

    modelo usuario creado

--- ammunation/models.py
+++ ammunation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario (models.Model):
+    nombre=models.CharField(max_length=50,null=False)
+    apellido=models.CharField(max_length=50,null=False)
+    correo=models.EmailField(unique=True,null=False)
+    contraseña=models.CharField(max_length=50,null=False)
+    
